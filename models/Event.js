fbaa98acd641e15dd481d70dd18c0cd881f08311
yarasamaha <yara_k_samaha@hotmail.com>
2019-02-23 22:42:09 +0200

    added classes for all entities, shalaby-arrays not yet finished

---
+++ models/Event.js
@@ -0,0 +1,14 @@
+class Event{
+    constructor(id,remaining_places,organizer,location,about,price,speakers,topics,attendees_ids)
+    {
+        this.id=id;
+        this.remaining_places=remaining_places;
+        this.organizer=organizer;
+        this.location=location;
+        this.about=about;
+        this.price=price;
+        this.speakers=speakers;
+        this.topics=topics;
+        this.attendees_ids=attendees_ids;
+    }
+}
--- shalaby_arrays.js
+++ shalaby_arrays.js
@@ -1,75 +1,19 @@
+
+const User = require('../../models/User')
+const Member = require('../../models/User')
+
 const users=[
-    {
-        id:'1',
-        first_name:"Youssef",
-        middle_name:"Zaki",
-        last_name:"Shalaby",
-        dob:"23/09/1998",
-        email:"youshalaby@gmail.com",
-        password:"allezleblues",
-        phone: '01119455455' ,
-        country:"Egypt",
-        city:"cairo",
-        account_open_on:""
+ 
+
+    new User('1',"Youssef","Zaki","Shalaby","23/09/1998","youshalaby@gmail.com","allezleblues",'01119455455',"Egypt","cairo","",true),
+    new User('2',"Mesut","AMin","Ozil","23/09/1990","ozil@gmail.com","iamjd",'02222345455',"England","London","",false),
+    new User('3',"Emily","Olivia","Blunt","23/02/1983","ms_blunt@gmail.com","holywood202",'9455667788',"USA","New Jersey","",false),
+    new User('4',"Tessa","Lyn","Thompson","03/10/1983","lifeline@gmail.com","blackpanther",'0104840022',"USA","New York","",false)  
+    ];
+    //makammeltsh from here
     
-    
-    
-    },
-    {
-        id:'2',
-        first_name:"Mesut",
-        middle_name:"AMin",
-        last_name:"Ozil",
-        dob:"23/09/1990",
-        email:"ozil@gmail.com",
-        password:"iamjd",
-        phone: '02222345455' ,
-        country:"England",
-        city:"London",
-        account_open_on:""
-    
-    
-    
-    },
-    {
-        id:'3',
-        first_name:"Emily",
-        middle_name:"Olivia",
-        last_name:"Blunt",
-        dob:"23/02/1983",
-        email:"ms_blunt@gmail.com",
-        password:"holywood202",
-        phone: '9455667788' ,
-        country:"USA",
-        city:"New Jersey",
-        account_open_on:""
-    
-    
-    
-    },
-    {
-        id:'4',
-        first_name:"Tessa",
-        middle_name:"Lyn",
-        last_name:"Thompson",
-        dob:"03/10/1983",
-        email:"lifeline@gmail.com",
-        password:"blackpanther",
-        phone: '0104840022' ,
-        country:"USA",
-        city:"New York",
-        account_open_on:""
-    
-    
-    
-    }
-    
-    ]
-    
-    const admin=[
-        {id:'1'}
-    ]
     const member=[
+
         {
             id:'2',
             years_of_experiece:'4',
