4a6fca919a73b977075ba5c58733a6f62faa03bd
farahabdelsalam <47149564+farahabdelsalam@users.noreply.github.com>
2019-02-23 23:58:18 +0200

    Add files via upload
    updated tables

---
+++ database/Consult_table.js
@@ -0,0 +1,5 @@
+import ConsultancyAgency from '/models/ConsultancyAgency';
+
+const consultancy_agency=[
+    new ConsultancyAgency('1',"Dell","Technology",["musk","Marco"],["google.docs/report_of_musk"])
+]
---
+++ database/User_tables.js
@@ -0,0 +1,23 @@
+import User from '/models/User';
+
+const users=[
+    new User('1',"Youssef","Zaki","Shalaby","23/09/1998","youshalaby@gmail.com","allezleblues",'01119455455',"Egypt","cairo","",true),
+    new User('2',"Mesut","AMin","Ozil","23/09/1990","ozil@gmail.com","iamjd",'02222345455',"England","London","",false),
+    new User('3',"Emily","Olivia","Blunt","23/02/1983","ms_blunt@gmail.com","holywood202",'9455667788',"USA","New Jersey","",false),
+    new User('4',"Tessa","Lyn","Thompson","03/10/1983","lifeline@gmail.com","blackpanther",'0104840022',"USA","New York","",false)  
+    ];
+
+    const member=[
+        
+           new Member('2',"Mesut","AMin","Ozil","23/09/1990","ozil@gmail.com","iamjd",'02222345455',"England","London","",'4',["Programming","Editing","Design"],["tech","computers","football"]),
+           new Member('3',"Emily","Olivia","Blunt","23/02/1983","ms_blunt@gmail.com","holywood202",'9455667788',"USA","New Jersey","",'7',["English","Arabic","Chinese"],["Translating","Cooking","Writing"])
+         
+    
+    ];
+    
+    const partner=[
+        
+            new Partner('3',"Emily","Olivia","Blunt","23/02/1983","ms_blunt@gmail.com","holywood202",'9455667788',"USA","New Jersey","",'1'),
+  
+        
+    ];
