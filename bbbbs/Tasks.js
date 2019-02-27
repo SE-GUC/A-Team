091825b5d235406d21349a68735ebe347dd33940
farahabdelsalam <47149564+farahabdelsalam@users.noreply.github.com>
2019-02-28 00:36:34 +0200

    Add files via upload

---
+++ bbbbs/Tasks.js
@@ -0,0 +1,44 @@
+const Tasks=[
+    {
+        id:'1', 
+        time_of_post:"21/02/2019 8:15 PM",
+        time_of_review:"",
+        monetary_compensation:'5678',
+        price:'20000',
+        time_of_assingment:"",
+        is_assigned:false,
+        assigned_id:'',
+        time_expected:"3 weeks",
+        level_of_comitment:"High",
+        is_reviewed: false,
+        experience_needed:'3',
+        description:"I need someone to Apply unit tests on my code",
+        p_id:'4',
+        skills:["Java","programming"],
+        response_from_admin:"",
+        admin_id:'1'
+
+    },
+    {
+        id:'2',
+        time_of_post:"21/02/2019 7:15 PM",
+        time_of_review:"",
+        monetary_compensation:'58',
+        price:'200',
+        time_of_assingment:"",
+        is_assigned:false,
+        assigned_id:'',
+        time_expected:"3 days",
+        level_of_comitment:"low",
+        is_reviewed: false,
+        experience_needed:'2',
+        description:"Translation from Chinese to English Needed",
+        p_id:'4',
+        skills:["Chinese","English","Translation"],
+        response_from_admin:"",
+        admin_id:'1'
+
+    }
+
+];
+module.exports = Tasks;
---
+++ bbbbs/notif.js
@@ -0,0 +1,11 @@
+const notif = [
+    {
+        memID: "2",
+        taskID: "1"
+    },
+    {
+        memID: "3",
+        taskID: "2"
+    }
+];
+module.exports = notif;
