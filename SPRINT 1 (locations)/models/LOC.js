da1c341db7b53f6c9a20ef4c015bb6e41459913b
Demerdo <47485815+Demerdo@users.noreply.github.com>
2019-02-28 13:04:43 +0200

    Add files via upload

---
+++ SPRINT 1 (locations)/models/LOC.js
@@ -0,0 +1,10 @@
+class LOC {
+    constructor(title, location, capacity, booked) {
+        this.title = title;
+        this.location = location;
+        this.capacity = capacity;
+        this.booked = booked;
+    };
+}
+
+module.exports = LOC
