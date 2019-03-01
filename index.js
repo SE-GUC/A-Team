b235b5e8191d0a1d28c6979638d233542bac366c
eng1998 <engykamel98@outlook.com>
2019-03-01 15:36:31 +0200

    final

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express=require('express');
-const applications=require('./routes/api/applications')
 
 const app = express();
 app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Please Apply</h1>
-    <a href="/api/applications">Applications</a>`);
+const http= require('http')
+const server= http.createServer(app);
+const events=require('./routes/api/events')
+const applications = require('./routes/api/applications')
+const locations = require('./routes/api/locations')
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send(`
+    <h1>Hello friend</h1>
+    <a href="/api/events" > to create an event</a>`)
 })
 
 
-app.use('/api/applications', applications)
+app.get('/', (req, res) => {
+    res.send(`<h1>Freelancer </h1>
+    <a href="/api/locations">Locations</a>
+       `);
+})
 
 
 
+app.use('/api/events', events)
+app.use('/api/locations', locations)
+app.use('/api/applications',applications)
+
+
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
+ 
