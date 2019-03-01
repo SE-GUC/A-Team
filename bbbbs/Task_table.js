dae46608ffc4eb6b0a70bd365079691df1177ed1
mohamedhooda <elhobbakhaless@gmail.com>
2019-03-01 20:15:17 +0200

    farah's folder

--- bbbbs/Task_table.js
+++
@@ -1,7 +0,0 @@
-import Task from '../models/Task';
-
-const Task=[
-    new Task('1', "21/02/2019 8:15 PM","",'5678','20000',"",false,'',"3 weeks","High",false,'3',"I need someone to Apply unit tests on my code",'4',["Java","programming"],"", '1') ,
-    new Task('2', "21/02/2019 7:15 PM","",'568','200',"",false,'',"3 days","Low",false,'3',"I need someone to Apply unit tests on my code",'4',["Chinese","English","Translation"],"", '1')  
-    
-];
---
+++ story1.5/Event_table.js
@@ -0,0 +1,6 @@
+import Event from '../models/Event';
+
+const event=[
+        new Event('4','12',"Mohammed Mahrous","Mall of arabia hall 2, 6 october city, cairo, Egypt","event that helps ict startups",'60',["Elon Musk","Hassan Soubra"],["technology","Java","Programming"],['1','2'])
+
+];
