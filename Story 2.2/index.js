486d7c1c718ce86efae63a1487cc2d18881e675b
Demerdo <47485815+Demerdo@users.noreply.github.com>
2019-03-01 12:58:08 +0200

    Add files via upload

---
+++ Story 2.2/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const locations = require('./routes/api/locations')
+
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Freelancer </h1>
+    <a href="/api/locations">Locations</a>
+       `);
+})
+
+
+
+
+app.use('/api/locations', locations)
+
+
+
+
+
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+const port = 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
+
