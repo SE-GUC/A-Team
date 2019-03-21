0da4843233759c3bc0591379030f1231e3eb8c14
yarasamaha <yara_k_samaha@hotmail.com>
2019-03-22 00:20:38 +0200

    user story 2.6 done and tested plus consultancy agency schrma

--- routes/api/users.js
+++ routes/api/users.js
@@ -162,6 +162,34 @@ router
   })
 
 
+  router
+  .route('/:id/addEvent')
+  .all(async (request, response, next) => {
+    const status = joi.validate(request.params, {
+      id: joi.string().length(24).required()
+    })
+    if (status.error) {
+      return response.json({ error: status.error.details[0].message })
+    }
+    next()
+  })
+  .post(async (request, response) => {
+    try {
+      const status = joi.validate(request.body, {
+        eventid:joi.string().length(24)
+      })
+      if (status.error) {
+        return response.json({ error: status.error.details[0].message })
+      }
+      
+      const user = await User.findByIdAndUpdate(request.params.id, { $push: { eventsAttended: request.body.eventid } }).exec()
+      return response.json({ data: user })
+    } catch (err) {
+      return response.json({ error: err.message })
+    }
+  })
+
+
 module.exports=router
     
     
