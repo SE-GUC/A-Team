fe2d4ea2d461df109cdcb3068ce0b7d09a823d5d
farahabdelsalam <farah.abdelsalam@hotmail.com>
2019-03-21 23:54:07 +0200

    notify member not tested

---
+++ routes/api/notify.js
@@ -0,0 +1,26 @@
+router
+  .route('/:id/notifyMember')
+  .all(async (request, response, next) => {
+    const status = joi.validate(request.params, {
+      id: joi.string().length(24).required()
+    })
+    if (status.error) {
+      return response.json({ error: status.error.details[0].message })
+    }
+    next()
+  })
+  .post(async (request, response) => {
+    try {
+      const status = joi.validate(request.body, {
+        taskid:joi.string().length(24)
+      })
+      if (status.error) {
+        return response.json({ error: status.error.details[0].message })
+      }
+      
+      const user = await User.findByIdAndUpdate(request.params.id, { $push: { tasksAssigned: request.body.taskid } }).exec()
+      return response.json({ data: user })
+    } catch (err) {
+      return response.json({ error: err.message })
+    }
+  })
