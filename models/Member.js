2010117ea6a25bef9e589daceb5580aade60860d
mshislam <47431781+mshislam@users.noreply.github.com>
2019-03-23 18:15:09 +0200

    Update Member.js

--- models/Member.js
+++ models/Member.js
@@ -4,18 +4,22 @@ const Schema = mongoose.Schema
 
 
 const MemberSchema = new Schema({
-    
-    years_of_experiece:{
+   
+    years_of_experience:{
         type:Number,
-        required:true
-    },
-    skills:[{
+        required: true
+    },skills:[{
         type:String,
-        required:true,
+        required:true
     }],
     interests:[{
         type:String,
-        required:true,
+        required:true
+    }],
+    notifications:[{
+        type:Number,
+        required:true
     }]
 })
-module.exports=Members= mongoose.model('members', MemberSchema)
+
+module.exports = Members = mongoose.model('Member', MemberSchema)
