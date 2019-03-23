ad742ee5a78ce04d87f0b986a7b0f098272285db
mshislam <47431781+mshislam@users.noreply.github.com>
2019-03-23 17:29:44 +0200

    Update Member.js

--- models/Member.js
+++ models/Member.js
@@ -1,12 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-class Member {
-    constructor(id,first_name,middle_name,last_name,dob,email,password,phone,country,city,account_open_on,years_of_experience,skills,interests)
-    {
-        super(id,first_name,middle_name,last_name,dob,email,password,phone,country,city,account_open_on,false);
-        this.years_of_experience=years_of_experience;
-        this.skills=skills;
-        this.interests=interests;
-    };
-}
 
-module.exports=Member
+
+const MemberSchema = new Schema({
+    id:{
+        type:Number,
+        required:true
+
+    },
+    years_of_experiece:{
+        type:Number,
+        required:true
+    },
+    skills:[{
+        type:String,
+        required:true,
+    }],
+    interests:[{
+        type:String,
+        required:true,
+    }]
+})
+module.exports=member= mongoose.model('members', MemberSchema)
