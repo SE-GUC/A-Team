69860639923c35a9c365e7c2eb0625c476e87365
Shalabyyy <youshalaby@gmail.com>
2019-03-27 19:04:29 +0200

    Added Tests for get requests, WORKING

--- tests/projFuncs.js
+++ tests/projFuncs.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
 const heroku='https://ateamse2.herokuapp.com/'
+const headers={
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT fefege...' 
+        
+}
 const functions = {
         add: (x,y) => x+y,
         createProject:async()=>{
@@ -7,18 +12,19 @@ const functions = {
                     project_name:'Running Jest on project CRUD',
                     partner_responsible: '5c96352dd744db39e4940f23',
                     consultancy_agency_sponsor:'5c9b331577541a0017be83de'
-                })
-               return project
+                },{headers:headers})
+               return project 
                       
             },
             getproject:async()=>{
-                const project= await axios.get('https://ateamse2.herokuapp.com/api/project/crud',{
-                params:{
-                "id":"5c9b3d2a6c59bf0017b4a481"  
-                 }   
-         }
-            )
-            return project
+              return  axios({
+                        method: 'get',
+                        url: 'https://ateamse2.herokuapp.com/api/project/crud',
+                        headers: {'Content-Type': 'application/json'}, 
+                        data: {
+                        id:'5c9b3d2a6c59bf0017b4a481'
+                        }
+                      });
         }
         
 };
