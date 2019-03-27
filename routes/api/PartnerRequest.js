764ae2d28c049ed5cfbb5d80083a2dd1a83b2e0b
MohamedHossam11 <mohamed.queena@student.guc.edu.eg>
2019-03-27 22:15:49 +0200

    minor change

--- routes/api/PartnerRequest.js
+++ routes/api/PartnerRequest.js
@@ -3,6 +3,8 @@ const router= express.Router();
 const eventrequest= require('../../models/EventRequests')
 const mongoose = require('mongoose')
 
+
+
     // var eventrequest=[
     //     {
     //         id: 1,
@@ -19,9 +21,12 @@ const mongoose = require('mongoose')
     //     }
     // ]
 
-    router.get('/geteventrequest', (req, res) => {
-        eventrequest.find().then(eventrequest=>res.send(eventrequest))
-    });
+    router.get('/geteventrequest', async (req,res) =>{
+        const e= await eventrequest.find()
+
+        res.json({data: e})
+    })
+    
     
     // router.post('/testadd',(req,res)=>
     // {
@@ -32,7 +37,7 @@ const mongoose = require('mongoose')
     //    })
        
     // })
-    router.post('/addrequest', async (req,res) => {
+    router.post('/testa', async (req,res) => {
         const organizer   = req.body.organizer
         const isAccepted= false
         //const user = await User.findOne({email})
@@ -50,11 +55,42 @@ const mongoose = require('mongoose')
         .catch(err => res.json({error: err.message}))
     })
 
+    router.post('/test', async (req,res) => {
+        const organizer   = req.body.organizer
+        const id= req.body.id
+        const isAccepted= false
+        //const user = await User.findOne({email})
+        //if(user) return res.status(400).json({error: 'Email already exists'})
+        
+        //const salt = bcrypt.genSaltSync(10)
+        //const hashedPassword = bcrypt.hashSync(password,salt)
+        const neweventrequest = new eventrequest({
+                organizer:"",
+                isAccepted
+            })
+        neweventrequest
+        .save()
+        .then(e => res.json({data: e}))
+        .catch(err => res.json({error: 'Can not create request'}))
+    })
+
+    router.post('/addrequest',(req,res)=>
+    {
+        const organizer= req.body.organnizer
+       const neweventrequest= new eventrequest({
+           _id:eventrequest.length+1,
+           organizer:organizer,
+           isAccepted: false
+       })
+       neweventrequest.save().then(eventreq => res.json({eventrequest: eventreq}))
+    })
+
+
 
     // router.delete('/deleterequest/:id', (req,res) =>
     // {
     //     const found= eventrequest.some(event =>event.id===parseInt(req.params.id));
-    //     if (found){
+    //     if (found){/
     //         res.json({msg:'Event deleted', 
     //         eventrequest : eventrequest.filter(event=>event.id!==parseInt(req.params.id))});
     //     }
