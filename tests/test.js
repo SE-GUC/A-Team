07139bb788f7f46057bb07cc3ac53c1fb7030541
AmrEbnNashaat <slayerxd@live.com>
2019-03-27 22:49:40 +0200

    Done with Consultancy Agency CRUD Testing, added jest for all components

---
+++ tests/test.js
@@ -0,0 +1,33 @@
+const funcs = require('./functions')
+const axios = require('axios')
+
+//AMR NASHAAT, Consultancy Agency CRUD testing
+
+test('testing agency get request', async() => {
+    //expect.assertions(1)
+    const res = await funcs.getAgencies()
+    expect(res.data[0]._id).toEqual('5c9bd0d9ac225200175df719')
+});
+
+test('agency put request', async() => {
+    const newInfo = 'UPDATING INFO'
+    const res = await funcs.updateAgencyInfo('5c9bd0d9ac225200175df719',newInfo)
+    expect(res.data.data.info).toEqual(newInfo)
+});
+
+test('agency post test', async() => {
+    const before = await funcs.getAgencies()
+    const x = funcs.postAgency()
+    const after = await funcs.getAgencies()
+    expect(x).toBeDefined()
+    //Our function returns the added agency, which is a defined agency
+});
+
+
+test('agency delete test', async() => {
+    const before = await funcs.getAgencies()
+    const v = funcs.deleteAgency('5c9bd7dbac225200175df723')
+    const after = await funcs.getAgencies()
+    expect(before.data.length-1).toBe(after.data.length)
+});
+
