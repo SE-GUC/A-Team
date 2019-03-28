7f6af4b2a06f81e2a674a911677cb94bc042b780
Ammar Yasser Hassan <ammaryasser107@gmail.com>
2019-03-28 15:08:05 +0200

    added sprint 0 grading

--- README.md
+++
@@ -1,3 +0,0 @@
-# A-Team
-WE LIT ON   https://ateamse.herokuapp.com/
-Also ON https://ateamse2.herokuapp.com/ (inclues late submissions)
--- routes/api/admins.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const admins=[
-    {id:'1'}
-]
-module.exports =router
