75eafdebfa96b39ef1f46b6177717ab90c318026
MohamedHooda <36062082+MohamedHooda@users.noreply.github.com>
2019-03-29 22:28:09 +0200

    switch to events

--- routes/api_using_objects/consutancyagencies.js
+++
@@ -1,7 +0,0 @@
-import ConsultancyAgency from '../../models/ConsultancyAgency';
-
-const consultancy_agency=[
-    new ConsultancyAgency('1',"Dell","Technology",["musk","Marco"],["google.docs/report_of_musk"])
-];
-
-module.exports= consultancyagencies;
