8cd124119da0da0ffb465e9f00fc74267357b9d6
farahabdelsalam <farah.abdelsalam@hotmail.com>
2019-03-30 17:29:12 +0200

    change

--- tests/functions.js
+++ tests/functions.js
@@ -18,18 +18,6 @@ const functions = {
         console.log(no.data.notifications)
         return no
 
-
-
-
-        // const n = await axios.post('http://localhost:4000/api/notify/'+id+'/notifyMember/', {notifications: taskd})
-        // return n.data
-        //i have an array esmo notifications in members, i wanna add haga fi ok 1 sec msh bedan walahy bas huwa 3ayez el names yko
-        //bas asl it's a post request? wala eh idu whats wrong
-        //asl i was confused bardo fel awel but hooda kaman kan amelha keda
-        //no n its ef put, you want to update, go on
-        //for now bas srryyy
-        //always save 
-
     },
 
 
