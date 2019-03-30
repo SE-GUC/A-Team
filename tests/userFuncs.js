ff81450474d9a82303cd320ccd26e522202f4f42
MohamedHooda <36062082+MohamedHooda@users.noreply.github.com>
2019-03-30 19:58:48 +0200

    addded tests for user crud

--- tests/userFuncs.js
+++ tests/userFuncs.js
@@ -1,30 +1,34 @@
 const axios = require('axios');
 
 const myFuncs = {
-        createEvent:async()=>{
-                    const newEvent=await axios.post('https://ateamse2.herokuapp.com/api/users', {                            
-                      remaining_places: 250,
-                      location: "5c9bff7f569b9a001796d40a",
-                      about: "MERN Programming Style",
-                      price: 55,
-                      speakers: ["Absalam", "Bill Gates"],
-                      topics: ["Python", "Java"],
-                      type: "Computer Engineering",
-                      partnerInitiated: "5c93cd1f1c9fe35274d2f624",
-                      request:"5c93cd1f1c9fe35274d2f624",
-                      attendees: ["5c93cd1f1c9fe35274d2f624","5c93cd1f1c9fe35274d2f624"]
-                    })
-                    return newEvent.data.data;
+        createUser:async()=>{
+                    const newUser=await axios.post('https://ateamse2.herokuapp.com/api/users/register', {                            
+                      name: "Tessa",
+                      email: "life1liane12311111111@gmail.com",
+                      password: "$2a$10$5tqIRQrrI9QiyrOAQAH6...x4vzpvbC0SWIzJgf2V1VszM7V7OEnu",
+                      dob: "1983-03-09T22:00:00.000Z",
+                      phone: 104840022,
+                      account_open_on: ""
+                      })
+                      console.log(newUser.data.data)
+                    return newUser.data.data;
+
           },
         getUsers: async () => {
           const users = await axios.get('https://ateamse2.herokuapp.com/api/users')
           console.log(users.data)
           return users
         },
-        updateEvent: async(id,name) => {
-          const updatedUser = await axios.put('https://ateamse.herokuapp.com/api/users/'+id+"", {name: name})
+        updatedUser: async(id,name) => {
+          const updatedUser = await axios.put('https://ateamse2.herokuapp.com/api/users/'+id+"", {name: name})
+          console.log(updatedUser.data.data)
           return updatedUser.data.data
-      }    
-};
-myFuncs.getUsers(); 
+      },
+      deleteUser: async(id) =>{
+          const deletedUser=await axios.delete('https://ateamse2.herokuapp.com/api/users/'+id)
+          console.log(deletedUser.data.data)
+          return deletedUser;
+      },
+  };
+  myFuncs.deleteUser('5c9e8dc52d814000175cb808');
 module.exports = myFuncs;
--- tests/users.test.js
+++ tests/users.test.js
@@ -9,52 +9,27 @@ test('First event remaining places should equal 12', async () => {
     const response =  await funcs.getUsers()
     expect(response.data[0].name).toBe("Tessa")
 });
-// test('Creating an event test',async ()=>{
-//   const result = await funcs.createEvent()
-//   const request={
-//         remaining_places: 250,
-//         location: "5c9bff7f569b9a001796d40a",
-//         about: "MERN Programming Style",
-//         price: 55,
-//         speakers: ["Absalam", "Bill Gates"],
-//         topics: ["Python", "Java"],
-//         type: "Computer Engineering",
-//         partnerInitiated: "5c93cd1f1c9fe35274d2f624",
-//         request:"5c93cd1f1c9fe35274d2f624",
-//         attendees: ["5c93cd1f1c9fe35274d2f624","5c93cd1f1c9fe35274d2f624"]
-//   }
-//   expect(result).toBeDefined()
-//   expect(result.remaining_places).toEqual(request.remaining_places)
-//   expect(result.location).toEqual(request.location)
-//   expect(result.about).toEqual(request.about)
-//   expect(result.price).toEqual(request.price)
-//   expect(result.speakers).toEqual(request.speakers)
-//   expect(result.topics).toEqual(request.topics)
-//   expect(result.type).toEqual(request.type)
-//   expect(result.partnerInitiated).toEqual(request.partnerInitiated)
-//   expect(result.request).toEqual(request.request)
-//   expect(result.attendees).toEqual(request.attendees)
-// })
+
+test('Creating a user test',async ()=>{
+  const result = await funcs.createUser()
+  const name="Tessa"
+  expect(result).toBeDefined()
+  expect(result.name).toEqual(name)
+})
 
 
-// test('Updating an event in the database', async() => {
-//   const remaining_places = 122
-//   const response = await funcs.updateEvent('5c9e33e5559ed00017ece5ea',remaining_places)
-//   expect(response.remaining_places).toEqual(remaining_places)
-// });
+test('Updating a user in the database', async() => {
+  const name = 'Tessa'
+  const response = await funcs.updatedUser('5c93d2a91c9fe35274d2f625',name)
+  expect(response.name).toEqual(name)
+});
 
-// test('Creating a new feedback in the feedbacks array', async() =>{
-//   const allEvents= await funcs.getEvents();
-//   const response = await funcs.addNewFeedback("5c93b78f1d4b8e5b48557ba0","5c9e33e5559ed00017ece5ea","comment");
-//   expect(allEvents.data.data[0].feedbacks.length).toEqual(response.data.data.feedbacks.length);
-// })
-
-
-// test('Creating a new application in the applications array', async() =>{
-//   const allEvents= await funcs.getEvents();
-//   const response = await funcs.addNewApplication("5c93b78f1d4b8e5b48557ba0","5c9e33e5559ed00017ece5ea",true);
-//   expect(allEvents.data.data[0].applicants.length).toEqual(response.data.data.applicants.length);
-// })
+test("Deletes a user",async()=>{
+    const result = await funcs.deleteUser('5c9e8dc52d814000175cb808')
+    
+    expect(result.data.data).toEqual(null)
+  })
+  
 
 
-
+  
