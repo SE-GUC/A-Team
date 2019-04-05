001223fa761ec83445adbccc4fddc66274b67654
yarasamaha <yara_k_samaha@hotmail.com>
2019-04-05 22:19:01 +0200

    yara's 2.6 react component added

--- eventapp/src/App.js
+++ eventapp/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './components/EventList';
+import ShowFeedbacks from './components/ShowFeedbacks';
 class App extends Component {
   state = {
     show:false,
+    showFeedbacks:false
   }
   
   // renderStory() {
@@ -18,6 +20,15 @@ class App extends Component {
     event.preventDefault();
     this.setState({
       show:true,
+      showFeedbacks:false
+
+    })
+  }
+  booleanShowFeedbacks = event => {
+    event.preventDefault();
+    this.setState({
+      show:false,
+      showFeedbacks:true
 
     })
   }
@@ -27,21 +38,37 @@ class App extends Component {
     return(
     <div>
     <h1>Events</h1>
-    <button type="ShowEvents" onClick={this.booleanShow}>Show Events</button>
+    <button type="ShowEvents" onClick={this.booleanShow}>Show Events</button> 
+    <button type="ShowEvents" onClick={this.booleanShowFeedbacks}>Show Feedbacks of Event</button> 
     <EventList/>
     </div>
       )
   }
 
+  renderFeedbacks() {
+    return(
+      <div>
+      <h1>Events</h1>
+      <button type="ShowEvents" onClick={this.booleanShow}>Show Events</button> 
+      <button type="ShowEvents" onClick={this.booleanShowFeedbacks}>Show Feedbacks of Event</button> 
+      <ShowFeedbacks/>
+      </div>
+        )
+  
+  }
   render() {
     if(this.state.show) {
       return this.renderEvents()
+    }
+    if (this.state.showFeedbacks) {
+      return this.renderFeedbacks();
     }
     return (
       <div>
       <h1>Events</h1>
       <button type="submit" onClick={this.booleanShow}>Show Events</button>
-      </div>
+      <button type="ShowEvents" onClick={this.booleanShowFeedbacks}>Show Feedbacks of Event</button> 
+        </div>
     )
   }
 }
