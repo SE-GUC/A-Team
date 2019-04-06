68012b88b9a2e77c670274f4d80a1b58ba1a4005
mshislam <47431781+mshislam@users.noreply.github.com>
2019-04-06 20:25:39 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,16 +7,20 @@ import TaskList from './components/TaskList'
 import TaskPostForm from './components/TaskPostForm'
 import Recommend from './components/Recommend';
 import Review from './components/StoryOnepointTwo';
+import Taskviewapplicants from './components/Taskviewapplicants'
 
 const routing=(
     <Router>
     <div>
+    
       <Route path="/task_control_panel" component={TaskControl} />
       <Route path="/get_tasks" component={TaskList}/>
       <Route path="/dummy" component={Dummy} />
       <Route path='/submit_task' component={TaskPostForm}/>
       <Route path="/recommend" component={Recommend} />
       <Route path="/Review" component={Review} />
+      <Route path="/view_applicants" component={Taskviewapplicants} />
+    
       
     </div>
   </Router>
