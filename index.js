01bc6a3f5b24f8ff9e391fef393bcb9e77ed3758
Shalabyyy <youshalaby@gmail.com>
2019-04-22 22:37:57 +0200

    Added get * in server

--- index.js
+++ index.js
@@ -34,12 +34,9 @@ const notif=require('./routes/api/notification')
         console.log('Error is: ', error);
     });
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+    app.get('*', function(req, res){
+        res.sendFile(__dirname + '/index.html');
+      });
 
 
 app.get('/',(req,res)=>{
